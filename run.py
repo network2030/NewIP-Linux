63f3b9df3d57dcbda08578cdcac681dde86badd7
Rohit M P <rohitmp99@gmail.com>
2021-03-01 12:38:07 +0530

    tmp

--- run.py
+++ run.py
@@ -22,13 +22,13 @@ config.set_value('assign_random_names', False)
 
 # TOPOLOGY
 #
-#               h2
+#               r2 ---- h2
 #              /
 #             /
-#   h1 ---- r1
+#   h1 ---- r1 
 #             \
 #              \
-#               h3
+#               r3 ---- h3
 #
 ####
 
@@ -40,39 +40,44 @@ h2 = Node('h2')
 h3 = Node('h3')
 #r = router
 r1 = Node('r1')
+r2 = Node('r2')
+r3 = Node('r3')
 
 # Create interfaces
 
 (r1_h1, h1_r1) = connect(r1, h1, interface1_name='r1_h1', interface2_name='h1_r1')
-(r1_h2, h2_r1) = connect(r1, h2, interface1_name='r1_h2', interface2_name='h2_r1')
-(r1_h3, h3_r1) = connect(r1, h3, interface1_name='r1_h3', interface2_name='h3_r1')
+(r2_h2, h2_r2) = connect(r2, h2, interface1_name='r2_h2', interface2_name='h2_r2')
+(r3_h3, h3_r3) = connect(r3, h3, interface1_name='r3_h3', interface2_name='h3_r3')
+(r1_r2, r2_r1) = connect(r1, r2, interface1_name='r1_r2', interface2_name='r2_r1')
+(r1_r3, r3_r1) = connect(r1, r3, interface1_name='r1_r3', interface2_name='r3_r1')
 
 # Set Addresses
 
 h1_r1.set_address('10.0.1.2/24')
 r1_h1.set_address('10.0.1.1/24')
-
-h2_r1.set_address('10.0.2.2/24')
-r1_h2.set_address('10.0.2.1/24')
-
-h3_r1.set_address('10.0.3.2/24')
-r1_h3.set_address('10.0.3.1/24')
-
-# h3_r1.set_address('2001:db8:3333:4444:5555:6666:7777:8888')
-# r1_h3.set_address('2001:db8:3333:4444:CCCC:DDDD:EEEE:FFFF')
+h2_r2.set_address('10.0.2.2/24')
+r2_h2.set_address('10.0.2.1/24')
+h3_r3.set_address('10.0.3.2/24')
+r3_h3.set_address('10.0.3.1/24')
+r1_r2.set_address('10.0.4.1/24')
+r2_r1.set_address('10.0.4.2/24')
+r1_r3.set_address('10.0.5.1/24')
+r3_r1.set_address('10.0.5.2/24')
 
 r1.enable_ip_forwarding()
+r2.enable_ip_forwarding()
+r3.enable_ip_forwarding()
 RoutingHelper(protocol='rip').populate_routing_tables()
 
 pp = pprint.PrettyPrinter(indent=4)
 
-def router_proc():
+def router_proc(iface):
     router_obj = router()
-    router_obj.start(iface='r1_h1')
+    router_obj.start(iface=iface)
 
-def receiver_proc():
+def receiver_proc(iface):
     receiver_obj = receiver()
-    receiver_obj.start(iface='h2_r1')
+    receiver_obj.start(iface=iface)
 
 
 def sender_proc():
@@ -84,22 +89,23 @@ def sender_proc():
 
 if os.system('make -C xdp/newip_router/') != 0:
     exit()
+
 with h1:
     os.system('./xdp/newip_router/xdp_loader --progsec xdp_pass --filename ./xdp/newip_router/xdp_prog_kern.o --dev h1_r1')
-with h2:
-    os.system('./xdp/newip_router/xdp_loader --progsec xdp_pass --filename ./xdp/newip_router/xdp_prog_kern.o --dev h2_r1')
 with r1:
     os.system('./xdp/newip_router/xdp_loader --progsec xdp_router --filename ./xdp/newip_router/xdp_prog_kern.o --dev r1_h1')
-    os.system('./xdp/newip_router/xdp_loader --progsec xdp_router --filename ./xdp/newip_router/xdp_prog_kern.o --dev r1_h2')
     os.system('sudo ./xdp/newip_router/xdp_prog_user -d r1_h1')
-    os.system('sudo ./xdp/newip_router/xdp_prog_user -d r1_h2')
+    os.system('./xdp/newip_router/xdp_loader --progsec xdp_pass --filename ./xdp/newip_router/xdp_prog_kern.o --dev r1_r2')
+with r2:
+    os.system('./xdp/newip_router/xdp_loader --progsec xdp_router --filename ./xdp/newip_router/xdp_prog_kern.o --dev r2_r1')
+    os.system('sudo ./xdp/newip_router/xdp_prog_user -d r2_r1')
 
 with h2:
-    receiver_process = multiprocessing.Process(target=receiver_proc)
+    receiver_process = multiprocessing.Process(target=receiver_proc, args=('h2_r2',))
     receiver_process.start()
 
 # with r1:
-#     router_process = multiprocessing.Process(target=router_proc)
+#     router_process = multiprocessing.Process(target=router_proc, args=('h1_r1',))
 #     router_process.start()
 
 # Ensure routers and receivers have started
